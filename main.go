1322be33a0d61e7d4c8a460a2de44863329bb1b3
Michael Wang <michael.icwang@gmail.com>
2020-02-22 14:54:41 +0800

    /db

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
 	"github.com/russross/blackfriday"
 )
 
@@ -20,6 +24,50 @@ func repeatHandler(r int) gin.HandlerFunc {
 		}
 		c.String(http.StatusOK, buffer.String())
 	}
+}
+
+func dbFunc(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error creating database table: %q", err))
+			return
+		}
+
+		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error incrementing tick: %q", err))
+			return
+		}
+
+		rows, err := db.Query("SELECT tick FROM ticks")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error reading ticks: %q", err))
+			return
+		}
+
+		defer rows.Close()
+		for rows.Next() {
+			var tick time.Time
+			if err := rows.Scan(&tick); err != nil {
+				c.String(http.StatusInternalServerError, fmt.Sprintf("Error scanning ticks: %q", err))
+				return
+			}
+			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+		}
+	}
+}
+
+func setupDB() (*sql.DB, error) {
+	var dsn string
+
+	_, local := os.LookupEnv("LOCAL")
+	if local {
+		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"))
+	} else {
+		dsn = os.Getenv("DATABASE_URL")
+	}
+
+	log.Printf("database dsn: %s", dsn)
+	return sql.Open("postgres", dsn)
 }
 
 func main() {
@@ -36,6 +84,12 @@ func main() {
 		repeat = 5
 	}
 
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+	defer db.Close()
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -50,6 +104,8 @@ func main() {
 	})
 
 	router.GET("/repeat", repeatHandler(repeat))
+
+	router.GET("/db", dbFunc(db))
 
 	router.Run(":" + port)
 }
--- run_local_windows.sh
+++ run_local_windows.sh
@@ -1,3 +1,2 @@
 go build -o bin/go-getting-started.exe -v
-set PORT=5000
 heroku local web
